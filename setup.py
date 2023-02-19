a9531d4b69b00049150d43952cbdabaa24ec7cd6
Sal <salmanfaris2005@hotmail.com>
2023-02-19 22:20:44 +0400

    Added .gitignore

---
+++ setup.py
@@ -0,0 +1,14 @@
+#  Copyright (c) 2023. THE FOLLOWING CODE IS NOT TO BE USED WITHOUT PERMISSION FROM THE AUTHOR.
+#  THE FOLLOWING CODE WAS WRITTEN FOR EDUCATIONAL PURPOSES.
+#  Author: Sal Faris
+#  Email: hz14n7os@duck.com
+#  Github: github.com/The-Sal
+from setuptools import setup, find_packages
+setup(
+    name='Vis',
+    version='0.0.1',
+    packages=find_packages(),
+    author='Sal Faris',
+    include_dirs=['binary'],
+    include_package_data=True
+)
